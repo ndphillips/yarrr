7e14da1c5a8f2dcf20be546a85bfc116e6bbb31a
Nathaniel Phillips <nathaniel.d.phillips.is@gmail.com>
2015-09-21 11:57:17 +0200

    test

---
+++ R/hsurvey_doc.R
@@ -0,0 +1,17 @@
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+#' 
+#' 
+#' 
+
+"hsurvey"
