d7ce5a6a913a9f4c5336e2a017d9ba8e0906084f
Nathaniel Phillips <nathaniel.d.phillips.is@gmail.com>
2015-09-21 10:51:49 +0200

    minor updates

---
+++ R/tanny_function.R
@@ -0,0 +1,19 @@
+#' Tanny function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' tanny_function()
+
+
+tanny_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love cats!")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
+
