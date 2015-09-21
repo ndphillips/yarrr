dcc194fb99a969fb5ac14b8dddd978059c592e48
Nathaniel Phillips <nathaniel.d.phillips.is@gmail.com>
2015-09-21 13:57:38 +0200

    test

---
+++ R/pirates_doc.R
@@ -0,0 +1,19 @@
+#' pirates
+#'
+#' A dataset containing the results of a survey and combat skills of 1,000 pirates
+#' diamonds.
+#'
+#' @format A data frame containing 1,000 rows and 11 columns
+#' \describe{
+#'   \item{id}{price, in US dollars}
+#'   \item{sex}{weight of the diamond, in carats}
+#'   \item{headband}{a yes or no string value indicating whether or not a pirate wears a headband}
+#'   \item{age}{age of the pirate in years}
+#'   \item{college}{the college the pirate went to}
+#'   ...
+#' }
+#' @source 1645 annual international pirate meeting
+#' 
+#' 
+#' 
+"pirates"
