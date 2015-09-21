b4dc8cf884d993be2eab18ba7698c259509ea8de
Nathaniel Phillips <nathaniel.d.phillips.is@gmail.com>
2015-09-21 12:06:42 +0200

    test

--- R/hsurvey_doc.R
+++ R/hsurvey_doc.R
@@ -1,17 +1,16 @@
-#' Prices of 50,000 round cut diamonds.
+#' Survey of pirates
 #'
 #' A dataset containing the prices and other attributes of almost 54,000
 #' diamonds.
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame containing results from a survey of 
 #' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{sex}{price, in US dollars}
+#'   \item{age}{weight of the diamond, in carats}
 #'   ...
 #' }
-#' @source \url{http://www.diamondse.info/}
+#' @source 1645 annual international pirate meeting
 #' 
 #' 
 #' 
-
 "hsurvey"
