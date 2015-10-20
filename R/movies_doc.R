30b76d84760d1e9ff6cd17a487c5f2ed9e1f8c2a
Nathaniel Phillips <nathaniel.d.phillips.is@gmail.com>
2015-10-20 12:35:39 +0200

    added movies dataset

---
+++ R/movies_doc.R
@@ -0,0 +1,30 @@
+#' movies
+#'
+#' A dataset containing the 5,000 top grossing movies (of all time?)
+#' 
+#'
+#' @format A data frame containing 5,000 rows and 14 columns
+#' \describe{
+#'   \item{name}{movie name}
+#'   \item{boxoffice.total}{Total box office revenue (in USD)}
+#'   \item{boxoffice.domestic}{Domestic box office revenue (in USD)}
+#'   \item{boxoffice.international}{International box office revenue (in USD)}
+#'   \item{dvd.domestic}{DVD revenue (in USD)}
+#'   \item{budget}{Production budget (in USD)}
+#'   \item{rating}{MPAA rating}
+#'   \item{genre}{Movie genre}
+#'   \item{creative.type}{Type of movie}
+#'   \item{time}{Running time (in minutes)}
+#'   \item{year}{Release year}
+#'   \item{production.method}{How the movie was produced}
+#'   \item{sequel}{Was the movie a sequel?}
+#'   \item{boxoffice.domestic.inflationadj}{Inflation adjusted domestic box office revenue (in USD)}
+#' 
+#'   
+#'   ...
+#' }
+#' @source 1645 annual international pirate meeting
+#' 
+#' 
+#' 
+"movies"
