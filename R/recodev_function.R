2c70daa69833c97d1647c2479c2a2c2ca82e5eab
Nathaniel Phillips <nathaniel.d.phillips.is@gmail.com>
2016-01-29 16:27:16 +0100

    asdf

--- R/recodev_function.R
+++ R/recodev_function.R
@@ -10,14 +10,14 @@
 #' @examples
 #'
 #' x <- c("y", "y", "XSF", "y", "0", "X", "0", "0", "y", "n", "0", "1", "1")
-#' recode.v(original.vector = x,
+#' recodev(original.vector = x,
 #'          old.values = c("y", "1", "n", "0"),
 #'          new.values = c(1, 1, 0, 0)
 #' )
 #'
 #'
 #' x <- c("y", "y", "XSF", "y", "0", "X", "0", "0", "y", "n", "0", "1", "1")
-#' recode.v(original.vector = x,
+#' recodev(original.vector = x,
 #'          old.values = c("y", "1", "n", "0"),
 #'          new.values = c(1, 1, 0, 0),
 #'          others = NA
