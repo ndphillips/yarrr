b475e49b4fcdc27aab6c5d72787ad9df9d99d714
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2016-04-07 15:44:27 +0200

    several minor updates to pirateplot

--- R/pirateplot_function.R
+++ R/pirateplot_function.R
@@ -2,21 +2,22 @@
 #'
 #' The pirateplot function creates an RDI plot (Raw data, Descriptive and Inferential statistic) pirate version of the fantastic beanplot function in the beanplot package. Just like a beanplot, pirateplot takes a discrete iv and a continuous dv, and creates a plot showing raw data, smoothed densities and central tendency. In addition, pirateplot adds the option for a 95% Highest Density Intervals (HDI), and has a few aesthetic differences preferred by pirates.
 #'
-#' @param formula (formula) A formula in the form dv ~ iv indicating the dv and iv to be plotted.
+#' @param formula (formula) A formula in the form y ~ x1 + x2 indicating the vertical response variable (y) and 1 or two independent varaibles
 #' @param data (dataframe) Data which to perform the beanplot on. This data can consist of dataframes, vectors and/or formulas. For each formula, a dataset can be specified with data=[dataset], and a subset can be specified with subset=[subset]. If subset/data arguments are passed, but there are not enough subset/data arguments, they are reused. Additionally, na.action, drop.unused.levels and xlev can be passed to model.frame in the same way. Also, parameters for axis and title can be passed.
 #' @param line.fun (function) A function that determines how average lines and bar heights are determined (default is mean).
 #' @param line.lwd (numeric) A number indicating the width of the average lines.
-#' @param pal (string) A string indicating the color palette of the plot. Can be a single color, or the name of a palette in the piratepal() function (e.g.; "basel", "google", "southpark")
-#' @param backcol (string) A string indicating the color of the plotting background
-#' @param point.cex,point.pch point.lwd (numeric) Numbers indicating the size, pch type, and line width of raw data points.
+#' @param pal (string) A string indicating the color palette of the plot. Can be a single color, a vector of colors, or the name of a palette in the piratepal() function (e.g.; "basel", "google", "southpark"). To see all the palettes, run piratepal(palette = "all", action = "show")
+#' @param gl.col (string) An optional string indicating the color of the horizontal gridlines.
+#' @param bar.border.col (string) An optional string indicating the color of the borders of the bars.
+#' @param back.col (string) A string indicating the color of the plotting background
+#' @param point.cex,point.pch,point.lwd (numeric) Numbers indicating the size, pch type, and line width of raw data points.
 #' @param width.min,width.max (numeric) The minimum and maximum width of a bean.
 #' @param cut.min, cut.max (numeric) Optimal minimum and maximum values of the beans.
-#' @param theme.o (integer) An integer in the set 1, 2, 3, 4 specifying an opacity theme (that is, specific values of bar.o, point.o, etc.). You can override specific opacity values in a theme by specifying bar.o, hdi.o (etc.)
-#' @param bar.o,point.o,hdi.o,line.o,bean.o (numeric) A number between 0 and 1 indicating how opaque to make the bars, points, hdi band, average line, and beans respectively. These values override
-#' @param hdi.iter (integer) An integer indicating how many iterations to run when calculating the HDI.
+#' @param theme.o (integer) An integer in the set 0, 1, 2, 3, specifying an opacity theme (that is, specific values of bar.o, point.o, etc.). You can override specific opacity values in a theme by specifying bar.o, hdi.o (etc.)
+#' @param bar.o,point.o,hdi.o,line.o,bean.o (numeric) A number between 0 and 1 indicating how opaque to make the bars, points, hdi band, average line, and beans respectively. These values override whatever is in the specified theme
+#' @param hdi.iter (integer) An integer indicating how many iterations to run when calculating the HDI. Larger values lead to better estimates, but can be (very) time consuming.
 #' @param jitter.val (numeric) A number indicaing how much to jitter the points horizontally. Defaults to 0.05.
 #' @param add (logical) A logical value indicating whether to add the pirateplot to an existing plotting space or not.
-
 #' @param ... other arguments passed on to the plot function (e.g.; main, xlab, ylab, ylim)
 #' @keywords plot
 #' @export
@@ -26,8 +27,7 @@
 #'
 #'# Pirateplots of the ChickWeight dataframe
 #'
-# Matrix of Pirate Plots of the ChickWeight dataframe
-#'
+#'# Matrix of Pirate Plots of the ChickWeight dataframe
 #'par(mfrow = c(4, 2))
 #'
 #'# Plot 1: Theme 1
@@ -36,7 +36,7 @@
 #'           data = ChickWeight,
 #'           main = "Theme 1",
 #'           theme.o = 1
-#')
+#'           )
 #'
 #'# Plot 2: Theme 1 + grayscale
 #'
@@ -45,7 +45,7 @@
 #'           main = "Theme 1 + grayscale",
 #'           theme.o = 1,
 #'           pal = "black"
-#')
+#'           )
 #'
 #'
 #'# Plot 3: Theme 2
@@ -54,9 +54,9 @@
 #'           data = ChickWeight,
 #'           main = "Theme 2",
 #'           theme.o = 2
-#')
+#'           )
 #'
-#'# Plot 4: Theme 2 + grayscale
+#'# Plot 4: Theme 2 + grayscale + gridlines
 #'
 #'pirateplot(formula = weight ~ Diet,
 #'           data = ChickWeight,
@@ -64,8 +64,9 @@
 #'           pal = "black",
 #'           theme.o = 2,
 #'           point.o = .2,
+#'           gl.col = gray(.9),
 #'           point.pch = 16
-#')
+#'           )
 #'
 #'
 #'# Plot 5: Theme 3
@@ -84,8 +85,9 @@
 #'           pal = "white",
 #'           theme.o = 3,
 #'           point.pch = 16,
+#'           gl.col = gray(.7),
 #'           back.col = gray(.2)
-#')
+#'           )
 #'
 #'# Plot 7: Theme 0 - Fully customised
 #'
@@ -100,7 +102,7 @@
 #'           point.pch = 16,
 #'           point.cex = 1.5,
 #'           jitter.val = .1
-#')
+#'           )
 #'
 #'
 #'# Plot 8: Theme 0\nFully customised
@@ -115,16 +117,69 @@
 #'           theme.o = 0,
 #'           back.col = transparent("steelblue4", .5),
 #'           line.lwd = 10,
+#'           yaxt = "n",   # no y-axis
+#'           ylab = "",
+#'           bty = "n",    # no plot border
 #'           point.pch = 16,
 #'           point.cex = 3,
 #'           jitter.val = .00
-#')
+#'           )
+#'
+#'par(mfrow = c(2, 2))
+#'
+#'# Matrix of plots with 2 IVs
+#'
+#'# Theme 1
+#'
+#'pirateplot(formula = weight ~ Diet + Time,
+#'           data = subset(ChickWeight, Time < 10),
+#'           theme.o = 1,
+#'           gl.col = gray(.8),
+#'           point.pch = 16,
+#'           main = "Two IVs\nTheme 1, default palette"
+#'           )
+#'
+#'# Theme 2
+#'
+#'pirateplot(formula = weight ~ Diet + Time,
+#'           data = subset(ChickWeight, Time < 10),
+#'           theme.o = 2,
+#'           pal = "basel",
+#'           point.pch = 16,
+#'           main = "Two IVs\nTheme 2, Basel palette"
+#'           )
+#'
+#'# Theme 3
+#'
+#'pirateplot(formula = weight ~ Diet + Time,
+#'           data = subset(ChickWeight, Time < 10),
+#'           theme.o = 3,
+#'           pal = "ipod",
+#'           point.pch = 16,
+#'           main = "Two IVs\nTheme 3 (with slow to calculate HDIs), ipod palette"
+#'           )
+#'
+#'# Theme 0 (fully customised)
+#'
+#'pirateplot(formula = weight ~ Diet + Time,
+#'           data = subset(ChickWeight, Time < 10),
+#'           theme.o = 0,
+#'           bar.o = 0,
+#'           point.o = .7,
+#'           bean.o = .2,
+#'           hdi.o = 0,
+#'           pal = "ipod",
+#'           point.pch = 16,
+#'           bar.border.col = gray(.5),
+#'           main = "Two IVs\nTheme 0, ipod palette"
+#'           )
 #'
 #'
-#'par(mfrow = c(1, 1))
-#'
-#'
-#'
+#
+
+
+
+
 
 pirateplot <- function(
   formula,
@@ -135,8 +190,8 @@ pirateplot <- function(
   point.cex = 1,
   point.pch = 1,
   point.lwd = 1,
-  cut.min = "",
-  cut.max = "",
+  cut.min = NULL,
+  cut.max = NULL,
   width.min = .3,
   width.max = NA,
   bean.o = NULL,
@@ -148,46 +203,61 @@ pirateplot <- function(
   hdi.iter = 1e3,
   jitter.val = .03,
   line.lwd = 4,
+  gl.col = NULL,
   ylim = NULL,
   xlim = NULL,
   xlab = NULL,
   ylab = NULL,
   main = NULL,
+  yaxt = NULL,
   add = F,
+  y.levels = NULL,
+  bar.border.col = NULL,
   ...
 ) {
 
 ## TESTING
 
-  #   jitter.val = .03
-  #   pal = "black"
-  #   line.fun = mean
-  #   background = 1
-  #   labels = ""
-  #   cex.lab = 1
-  #   point.cex = 1
-  #   point.pch = 1
-  #   point.lwd = 1
-  #   cut.min = ""
-  #   cut.max = ""
-  #   width.max = .45
-  #   width.min = .2
-  #   bean.o = .5
-  #   point.o = .5
-  #   bar.o = .5
-  #   hdi.o = 1
-  #   line.o = .5
-  #   hdi.iter = 1e3
-  #   ylim = ""
-  #   xlim = ""
-  #
-  #
-  #
-  #
-  #
-  # formula = weight ~ Diet
-  # line.fun = mean
-  # data = ChickWeight
+#
+#
+#
+#   line.fun = mean
+#   pal = "appletv"
+#   back.col = gray(1)
+#   point.cex = 1
+#   point.pch = 1
+#   point.lwd = 1
+#   cut.min = ""
+#   cut.max = ""
+#   width.min = .3
+#   width.max = NA
+#   bean.o = NULL
+#   point.o = NULL
+#   bar.o = NULL
+#   hdi.o = NULL
+#   line.o = NULL
+#   theme.o = 1
+#   hdi.iter = 1e3
+#   jitter.val = .03
+#   line.lwd = 4
+#   ylim = NULL
+#   xlim = NULL
+#   xlab = NULL
+#   ylab = NULL
+#   main = NULL
+#   add = F
+#   y.levels = NULL
+#
+#
+#
+#   formula = age ~ favorite.pirate
+#   data = pirates
+#   xlab = "Favorite Pirate"
+#   ylab = "Age"
+#   bty = "n"
+#
+#
+
 
 
   data.2 <- model.frame(formula = formula,
@@ -319,12 +389,77 @@ pirateplot <- function(
   }
 
 
+  if(is.null(bar.border.col)) {bar.border.col <- bar.col}
+  if(is.null(bar.border.col) == F) {bar.border.col <- rep(bar.border.col, times = length(bar.col))}
+
+
 
   # Create plotting space
-  total.dv.sd <- sd(dv.v)
 
-  if(is.null(ylim)) {ylim <- c(min(dv.v) - total.dv.sd * .5, max(dv.v) + total.dv.sd * .5)}
+# Determine y limits (y axis limits)
+  # y axis breaks (y.levels)
+
+  if(is.null(ylim) == TRUE & is.null(y.levels) == TRUE) {
+
+    # Determine best step size
+
+    steps.p <- c(1, 2, 5, 10, 25, 50, 100,
+                 seq(1e2, 1e3, 1e2),
+                 seq(1e3, 1e4, 1e3),
+                 seq(1e4, 1e5, 1e4),
+                 seq(1e5, 1e6, 1e5),
+                 seq(1e6, 1e7, 1e6),
+                 seq(1e7, 1e8, 1e7),
+                 seq(1e8, 1e9, 1e8))
+
+
+    range <- max(dv.v) - min(dv.v)
+
+    steps.p.m <- range / steps.p
+    best.step.size <- steps.p[which(abs(steps.p.m - 10) == min(abs(steps.p.m - 10)))]
+
+    plot.min <- floor(min(dv.v) / best.step.size) * best.step.size
+    plot.max <- plot.min + 10 * best.step.size
+
+    ylim <- c(plot.min, plot.max)
+    y.levels <- seq(plot.min, plot.max, by = best.step.size)
+
+  }
+
+  if(is.null(ylim) == FALSE & is.null(y.levels) == TRUE) {
+
+
+    steps.p <- c(1, 2, 5, 10, 25, 50, 100,
+                 seq(1e2, 1e3, 1e2),
+                 seq(1e3, 1e4, 1e3),
+                 seq(1e4, 1e5, 1e4),
+                 seq(1e5, 1e6, 1e5),
+                 seq(1e6, 1e7, 1e6),
+                 seq(1e7, 1e8, 1e7),
+                 seq(1e8, 1e9, 1e8))
+
+    range <- ylim[2] - ylim[1]
+
+    steps.p.m <- range / steps.p
+    best.step.size <- steps.p[which(abs(steps.p.m - 10) == min(abs(steps.p.m - 10)))]
+
+    plot.min <- floor(ylim[1] / best.step.size) * best.step.size
+    plot.max <- ylim[1] + 10 * best.step.size
+
+    y.levels <- seq(ylim[1], ylim[2], by = best.step.size)
+
+  }
+
+  if(is.null(ylim) == TRUE & is.null(y.levels) == FALSE) {
+
+    ylim <- c(min(y.levels), max(y.levels))
+
+    }
+
+
   if(is.null(xlim)) {xlim <- c(min(bean.loc) - .5, max(bean.loc) + .5)}
+
+
 
   # Determine x and y labels
 
@@ -333,6 +468,7 @@ pirateplot <- function(
 
 
   if(add == F) {
+
     par(mar = c(5, 4, 4, 1) + .1)
 
   plot(1,
@@ -340,17 +476,45 @@ pirateplot <- function(
        ylim = ylim,
        type = "n",
        xaxt = "n",
+       yaxt = "n",
        xlab = xlab,
        ylab = ylab,
        main = main,
+       yaxt = yaxt,
        ...
   )
+
+
+  # Add y-axis
+
+  if(is.null(yaxt)) {
+
+  axis(side = 2,
+       at = y.levels,
+       las = 1,
+       lwd = 1,
+       lwd.ticks = 1)
+
+  }
 
   # Add background
 
     rect(-1000, -1000, 10000, 10000, col = back.col)
 
+  # Add gridlines
+
+  if(is.null(gl.col) == F) {
+
+    abline(h = seq(min(y.levels),
+                   max(y.levels),
+                   length.out = 21),
+           lwd = c(.75, .25),
+           col = gl.col)
   }
+
+  }
+
+
 
 #    y.range <- max(dv.v) + total.dv.sd * .5 - min(dv.v) - total.dv.sd * .5
 #
@@ -377,8 +541,8 @@ pirateplot <- function(
          x.loc.i + width.max,
          fun.val,
          col = bar.col[bean.i],
-         border = bar.col[bean.i],
-         lwd = 0
+         border = bar.border.col[bean.i],
+         lwd = 1
     )
 
 
@@ -413,7 +577,7 @@ pirateplot <- function(
       dens.x.plot.i <- dens.x.i
       dens.y.plot.i <- dens.y.i
 
-      if(is.numeric(cut.min)) {
+      if(is.null(cut.min) == F) {
 
         dens.x.plot.i <- dens.x.i[dens.x.i > cut.min]
         dens.y.plot.i <- dens.y.i[dens.x.i > cut.min]
@@ -421,7 +585,7 @@ pirateplot <- function(
       }
 
 
-      if(is.numeric(cut.max)) {
+      if(is.null(cut.max) == F) {
 
         dens.x.plot.i <- dens.x.i[dens.x.i < cut.max]
         dens.y.plot.i <- dens.y.i[dens.x.i < cut.max]
