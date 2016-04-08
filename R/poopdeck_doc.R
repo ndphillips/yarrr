b9503b6365be072da125ab9af1f78d4ba79e4e31
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2016-04-08 16:38:34 +0200

    added poopdeck

---
+++ R/poopdeck_doc.R
@@ -0,0 +1,16 @@
+#' poopdeck
+#'
+#' A dataframe containing the amount of time it took to clean both pirate and shark poop from the poop deck using one of three different cleaning solutions
+#'
+#'
+#' @format A data frame containing 300 rows and 3 columns
+#' \describe{
+#'   \item{cleaner}{(string) - The cleaning solution used}
+#'   \item{type}{(string) - The type of poop being cleaned}
+#'   \item{time}{(numeric) - The amount of time (in minutes) the cleaning took.}
+#'  }
+#' @source 2015 annual international pirate meeting at the Bodensee in Konstanz, Germany
+#'
+#'
+#'
+"poopdeck"
---
+++ R/poopdeck_gen.R
@@ -0,0 +1,36 @@
+# poopdeck
+
+set.seed(100)
+cleaner <- rep(c("a", "b", "c"), each = 100)
+type <- rep(c("pirate", "shark"), times = 150)
+
+
+cleaner.num <- rep(0, 300)
+
+cleaner.num[cleaner == "a"] <- 20
+cleaner.num[cleaner == "b"] <- 15
+cleaner.num[cleaner == "c"] <- 20
+
+type.num <- rep(0, 300)
+
+type.num[type == "pirate"] <- 5
+type.num[type == "shark"] <- 30
+
+int.num <- rep(0, 300)
+int.num[cleaner == "a" & type == "pirate"] <- -10
+int.num[cleaner == "a" & type == "shark"] <- 10
+
+int.num[cleaner == "c" & type == "pirate"] <- 0
+int.num[cleaner == "c" & type == "shark"] <- -15
+
+
+time.mean <- cleaner.num + type.num + int.num
+time <- round(rnorm(300, mean = time.mean + 30, sd = 20), 1)
+
+poopdeck <- data.frame(cleaner = cleaner,
+                       type = type,
+                       time = time,
+                       stringsAsFactors = F)
+
+
+save(poopdeck, file = "data/poopdeck.RData")
