6ee5f9a8639154337809e25698a3f0c1f8f713da
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2016-04-08 16:58:20 +0200

    minor

--- R/poopdeck_doc.R
+++ R/poopdeck_doc.R
@@ -3,8 +3,9 @@
 #' A dataframe containing the amount of time it took to clean both pirate and shark poop from the poop deck using one of three different cleaning solutions
 #'
 #'
-#' @format A data frame containing 300 rows and 3 columns
+#' @format A data frame containing 300 rows and 4 columns
 #' \describe{
+#'   \item{day}{(integer) - The day that the poop was cleaned (1 through 300)}
 #'   \item{cleaner}{(string) - The cleaning solution used}
 #'   \item{type}{(string) - The type of poop being cleaned}
 #'   \item{time}{(numeric) - The amount of time (in minutes) the cleaning took.}
--- R/poopdeck_gen.R
+++ R/poopdeck_gen.R
@@ -1,38 +1,40 @@
 # poopdeck
 
 set.seed(200)
-cleaner <- rep(c("a", "b", "c"), each = 100)
-type <- rep(c("pirate", "shark"), times = 150)
+
+poopdeck <- data.frame(cleaner = rep(c("a", "b", "c"), times = 200),
+                        type = rep(c("pirate", "shark"), each = 3, times = 100),
+                       day = rep(1:100, each = 6),
+                       stringsAsFactors = F
+                        )
 
 
-cleaner.num <- rep(0, 300)
 
-cleaner.num[cleaner == "a"] <- 20
-cleaner.num[cleaner == "b"] <- 15
-cleaner.num[cleaner == "c"] <- 20
+cleaner.num <- rep(0, 600)
 
-type.num <- rep(0, 300)
+cleaner.num[poopdeck$cleaner == "a"] <- 20
+cleaner.num[poopdeck$cleaner == "b"] <- 15
+cleaner.num[poopdeck$cleaner == "c"] <- 20
 
-type.num[type == "pirate"] <- 5
-type.num[type == "shark"] <- 30
+type.num <- rep(0, 600)
 
-int.num <- rep(0, 300)
-int.num[cleaner == "a" & type == "pirate"] <- -10
-int.num[cleaner == "a" & type == "shark"] <- 10
+type.num[poopdeck$type == "pirate"] <- 5
+type.num[poopdeck$type == "shark"] <- 30
 
-int.num[cleaner == "c" & type == "pirate"] <- 0
-int.num[cleaner == "c" & type == "shark"] <- -15
+int.num <- rep(0, 600)
+int.num[poopdeck$cleaner == "a" & poopdeck$type == "pirate"] <- -10
+int.num[poopdeck$cleaner == "a" & poopdeck$type == "shark"] <- 10
+
+int.num[poopdeck$cleaner == "c" & poopdeck$type == "pirate"] <- 0
+int.num[poopdeck$cleaner == "c" & poopdeck$type == "shark"] <- -15
 
 
 time.mean <- cleaner.num + type.num + int.num
-time <- round(rnorm(300, mean = time.mean + 30, sd = 20), 1)
+time <- round(rnorm(600, mean = time.mean + 30, sd = 20), 0)
 
-poopdeck <- data.frame(cleaner = cleaner,
-                       type = type,
-                       time = time,
-                       stringsAsFactors = F)
+poopdeck$time <- time
 
-poopdeck <- poopdeck[sample(1:nrow(poopdeck)),]
-rownames(poopdeck) <- 1:nrow(poopdeck)
+poopdeck <- poopdeck[c("day", "cleaner", "type", "time")]
+
 
 save(poopdeck, file = "data/poopdeck.RData")
