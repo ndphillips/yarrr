1a432bfd9796d4729f0ad50643dd2193167d05da
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2016-04-15 11:45:04 +0200

    changed diamonds

--- R/diamonds_gen.R
+++ R/diamonds_gen.R
@@ -10,12 +10,13 @@ clarity <- round(rnorm(N, mean = 1, sd = .2), 2)
 
 color <- round(rnorm(N, mean = 5, sd = 1), 0)
 
-value <- round(2 * weight + 20 * clarity + rnorm(N, mean = N, sd = 20), 1)
+value <- round(2 * weight + 20 * clarity + rnorm(N, mean = N, sd = 5), 1)
 
 diamonds <- data.frame(weight, clarity, color, value)
 
-#summary(lm(value ~., data = diamonds))
-
+# summary(lm(value ~., data = diamonds))
+# plot(value, lm(value ~., data = diamonds)$fitted.values)
+# abline(a = 0, b = 1)
 
 save(diamonds, file = "data/diamonds.RData")
 
