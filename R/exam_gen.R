c25b9ab14132609fbf784921d500979441cc051a
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2016-05-04 11:32:21 +0200

    update examscores

--- R/exam_gen.R
+++ R/exam_gen.R
@@ -1,14 +1,14 @@
 
 # auction_gen
 
-set.seed(100)
+set.seed(101)
 
 N <- 100
 
 examscores <- data.frame(
-  a = round(rnorm(N, mean = 50, sd = 10), 0),
+  a = round(rnorm(N, mean = 50, sd = 20), 0),
   b = round(rnorm(N, mean = 30, sd = 5), 0),
-  c = round(rnorm(N, mean = 80, sd = 10), 0),
+  c = round(rnorm(N, mean = 70, sd = 10), 0),
   d = round(rnorm(N, mean = 40, sd = 5), 0)
 )
 
