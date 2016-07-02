15976b5017435feed4a9df0cccef3a25ece045a9
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2016-07-02 14:49:58 +0200

    bug

--- R/zzz.R
+++ R/zzz.R
@@ -11,6 +11,6 @@
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("************\nWelcome to YaRrr! The Pirate's Guide to R")
-  packageStartupMessage("Run yarrr() to see the main package guide")
+  packageStartupMessage("Run yarrr.guide() to see the main package guide")
   packageStartupMessage("Email me at yarrr.book@gmail.com with questions, comments, or movie recommendations\n************")
 }
