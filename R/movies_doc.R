377dba12cb72ef6e19685544395ca601e31b70ec
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2016-08-23 07:42:06 +0200

    Added movies dataset back to package
    Had to remove non-ASCII characters. Was easier to do than I thought…

---
+++ R/movies_doc.R
@@ -0,0 +1,26 @@
+#' movies
+#'
+#' A dataframe containing information about the top 5000 grossing movies of all time.
+#'
+#'
+#' @format A data frame containing 5000 rows and 13 columns
+#' \describe{
+#'   \item{name}{Movie name}
+#'   \item{rating}{MPAA rating}
+#'   \item{genre}{Movie genre}
+#'   \item{creative.type}{Creative type}
+#'   \item{time}{Running time in minutes}
+#'   \item{year}{Year of release}
+#'   \item{production.method}{Production method}
+#'   \item{sequel}{Was the movie a sequel? 1 = yes, 0 = no}
+#'   \item{budget}{Movie budget (in $USD millions)}
+#'   \item{revenue.all}{Gross worldwide revenue in $USD millions}
+#'   \item{revenue.dom}{Domestic revenue in $USD millions}
+#'   \item{revenue.int}{International revenue in $USD millions}
+#'   \item{revenue.inf}{Inflation adjusted worldwide revenue in $USD millions}
+#'  }
+#' @source www.the-numbers.com
+#'
+#'
+#'
+"movies"
