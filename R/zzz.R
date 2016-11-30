0bdf49970da549c30525c216015de9851e490329
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2016-11-30 18:37:07 +0100

    Updated pacakge startup message with citation info

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("************\nWelcome to yarrr v0.1.3!")
-  packageStartupMessage("Run yarrr.guide() to see the main package guide")
-  packageStartupMessage("Email me at yarrr.book@gmail.com with questions, comments, or movie recommendations\n************")
+  packageStartupMessage("yarrr v0.1.3. Citation info at citation('yarrr'). Package guide at yarrr.guide()")
+  packageStartupMessage("Email me at Nathaniel.D.Phillips.is@gmail.com")
 }
