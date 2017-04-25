2c291b922d822a7820b627dbaffec12345e9e484
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2017-04-25 10:16:02 +0200

    minor theme updates

--- R/pirateplot_function.R
+++ R/pirateplot_function.R
@@ -815,7 +815,7 @@ if(theme == 1) {
   if(is.null(bean.b.col)) {bean.b.col <- "black"}
   if(is.null(point.cex)) {point.cex <- .7}
   if(is.null(point.col)) {point.col <- "black"}
-  if(is.null(inf.b.col)) {inf.b.col <- "black"}
+  if(is.null(inf.b.col)) {inf.b.col <- gray(0)}
   if(is.null(inf.f.col)) {inf.f.col <- "white"}
   if(is.null(bean.lwd)) {bean.lwd <- 2}
   if(is.null(avg.line.col)) {avg.line.col <- "black"}
@@ -841,6 +841,7 @@ if(theme == 2) {
   if(is.null(bean.f.o)) {bean.f.o <- 1}
   if(is.null(inf.f.o)) {inf.f.o <- .6}
   if(is.null(inf.b.o)) {inf.b.o <- .8}
+  if(is.null(inf.b.col)) {inf.b.col <- gray(0)}
   if(is.null(avg.line.o)) {avg.line.o <- 1}
   if(is.null(bar.f.o)) {bar.f.o <- 0}
   if(is.null(bar.b.o)) {bar.b.o <- 0}
@@ -874,7 +875,7 @@ if(theme == 3) {
 
   if(is.null(bean.b.col)) {bean.b.col <- "black"}
   if(is.null(inf.f.col)) {inf.f.col <- "white"}
-  if(is.null(inf.b.col)) {inf.b.col <-  "black"}
+  if(is.null(inf.b.col)) {inf.b.col <-  gray(0)}
   if(is.null(avg.line.col)) {avg.line.col <- "black"}
   if(is.null(point.col)) {point.col <- "black"}
   if(is.null(point.cex)) {point.cex <- .5}
@@ -902,7 +903,7 @@ if(theme == 4) {
   if(is.null(bar.b.o))  {bar.b.o <- 1}
 
   if(is.null(inf.f.col)) {inf.f.col <- "black"}
-  if(is.null(inf.b.col)) {inf.b.col <-  "black"}
+  if(is.null(inf.b.col)) {inf.b.col <-  gray(0)}
   if(is.null(avg.line.col)) {avg.line.col <- "black"}
   if(is.null(bar.f.col)) {bar.f.col <- "white"}
   if(is.null(bar.b.col)) {bar.b.col <- "black"}
