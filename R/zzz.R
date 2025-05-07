a0624bbde4c55bd8279283be2cbb727c04cffd60
Nathaniel Phillips <nathaniel.d.phillips.is@gmail.com>
2025-05-07 16:17:11 -0700

    Removed startup message. Changed bad URL. Updated to v0.1.7

--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("yarrr v0.1.6. Citation info at citation('yarrr'). Package guide at yarrr.guide()")
-  packageStartupMessage("Email me at Nathaniel.D.Phillips.is@gmail.com")
-}
